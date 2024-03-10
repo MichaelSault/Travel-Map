d223bb7c2a0178b7633f0ad51190bea41674f088
MichaelSault <michael_sault@hotmail.com>
2024-03-09 23:58:50 -0500

    worked with labeling the world map, learning the library

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -1,6 +1,6 @@
 import '../App.css';
 import React from 'react';
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import { Annotation, ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
 
 function Home() {
 
@@ -14,6 +14,42 @@ function Home() {
             ))
             }
           </Geographies>
+          <Annotation
+            subject={[2.3522, 48.8566]}
+            dx={-40}
+            dy={-5}
+            connectorProps={{
+              stroke: "#FF5533",
+              strokeWidth: 2,
+              strokeLinecap: "round"
+            }}
+          >
+            <text x="-2" textAnchor="end" alignmentBaseline="middle" fill="#F53">
+              {"Paris"}
+            </text>
+          </Annotation>
+          <Annotation
+            subject={[-79.383, 43.653]}
+            dx={40}
+            dy={8}
+            connectorProps={{
+              stroke: "#FF5533",
+              strokeWidth: 2,
+              strokeLinecap: "round"
+            }}
+          >
+            <text x="-2" textAnchor="start" alignmentBaseline="middle" fill="#F53">
+              {"Toronto"}
+            </text>
+          </Annotation>
+          <Marker key={'Los Angeles'} coordinates={[-118.2426, 34.0549]}>
+            <circle r={2} fill="#F00" stroke="#fff" strokeWidth={0.5} />
+            <text
+              textAnchor="middle"
+              y={-20}
+              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            ></text>
+          </Marker>
         </ComposableMap>       
     </>
   )
